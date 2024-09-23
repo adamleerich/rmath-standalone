f73dc0c682768c9931f05ecb374b765db2b23e63
Adam Rich <adamleerich@pm.me>
2024-09-23 12:38:56 -0400

    Re-wrote RNG functions to use .GlobalEnv to store seeds

--- _sandbox/rng-didikeva/test-mt-functions.R
+++ _sandbox/rng-didikeva/test-mt-functions.R
@@ -1,73 +1,47 @@
 source('mt-functions.R')
 
-seed <- sample(-100:100, 1)
+set.seed(1)
+set_seed_alt(1)
+all(.Random.seed == .Random.seed.alt)
 
-# At set.seed R stores RNG state in .Random.seed
-# However, it is considered already spent
-# So, compare it pre-twist
-set.seed(
-  seed = seed, 
-  kind = 'Mersenne-Twister', 
-  normal.kind = "Inversion", 
-  sample.kind = "Rejection")
+u1 <- runif(1)
+u2 <- runif_alt(1)
+all(.Random.seed == .Random.seed.alt)
 
-R_seeds <- .Random.seed[3:626]
-my_seeds <- init_seeds__R(seed)
-all(R_seeds == my_seeds)
+for (i in 1:10) {
+  N <- 100e3
+  message("Iteration i = ", i)
+  message("  .Random.seed[2] = ", .Random.seed[2])
+  message("  .Random.seed.alt[2] = ", .Random.seed.alt[2])
+  message("  sum(.Random.seed) = ", sum(.Random.seed))
+  message("  sum(.Random.seed.alt) = ", sum(.Random.seed.alt))
+  u3 <- runif(N)
+  u4 <- runif_alt(N)
+  message("  all(u3 == u4) = ", all(u3 == u4))
+  message(
+    "  all(.Random.seed == .Random.seed.alt) = ", 
+    all(.Random.seed == .Random.seed.alt) )
+  
+}
 
-# Force R to get an unspent seed vector by asking for a random value
-# Then compare to an unspent vector manually created
-set.seed(seed)
-invisible(runif(1))
-R_seeds_twisted <- .Random.seed[3:626]
-my_seeds_twisted <- init_seeds(seed)
-all(R_seeds_twisted == my_seeds_twisted)
-
-
-
-
-
-# Check unif random variables
-set.seed(10)
-R_runif <- runif(624)
-my_seeds <- init_seeds(10)
-my_runif <- runif_from_seeds(my_seeds)
-all(R_runif == my_runif)
-
-R_runif <- runif(624)
-my_seeds <- twist_seeds(my_seeds)
-my_runif <- runif_from_seeds(my_seeds)
-all(R_runif == my_runif)
-
-R_runif <- runif(624)
-my_seeds <- twist_seeds(my_seeds)
-my_runif <- runif_from_seeds(my_seeds)
-all(R_runif == my_runif)
+system.time(runif(1e6))
+system.time(runif_alt(1e6))
 
 
-
-# What about fractional seeds?
-seed <- 20.5
-set.seed(seed)
-R_seeds <- .Random.seed[3:626]
-my_seeds <- init_seeds__R(seed)
-all(R_seeds == my_seeds)
-
-seed <- -891.7
-set.seed(seed)
-R_seeds <- .Random.seed[3:626]
-my_seeds <- init_seeds__R(seed)
-all(R_seeds == my_seeds)
-
-seed <- -2.1
-set.seed(seed)
-R_seeds <- .Random.seed[3:626]
-my_seeds <- init_seeds__R(seed)
-all(R_seeds == my_seeds)
-
-seed <- -123.5
-set.seed(seed)
-R_seeds <- .Random.seed[3:626]
-my_seeds <- init_seeds__R(seed)
-all(R_seeds == my_seeds)
-
+for (i in 1:10) {
+  N <- 100e3
+  min <- -1 * i
+  max <- i * 10
+  message("Iteration i = ", i)
+  message("  .Random.seed[2] = ", .Random.seed[2])
+  message("  .Random.seed.alt[2] = ", .Random.seed.alt[2])
+  message("  sum(.Random.seed) = ", sum(.Random.seed))
+  message("  sum(.Random.seed.alt) = ", sum(.Random.seed.alt))
+  u3 <- runif(N, min, max)
+  u4 <- runif_alt(N, min, max)
+  message("  all(u3 == u4) = ", all(u3 == u4))
+  message(
+    "  all(.Random.seed == .Random.seed.alt) = ", 
+    all(.Random.seed == .Random.seed.alt) )
+  
+}
